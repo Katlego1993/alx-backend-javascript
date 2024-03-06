3ddf16a5dda9b98f1053e6b056c9126cc4125188
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2024-03-06 15:30:46 +0000

    all

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,29 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-// Defining the async function
-async function asyncUploadUser() {
+const asyncUploadUser = async () => {
   try {
-    // Using Promise.allSettled to handle both promises and catch any errors
-    const results = await Promise.allSettled([
-      uploadPhoto(/* provide the necessary arguments */),
-      createUser(/* provide the necessary arguments */),
-    ]);
-
-    // Extracting the responses or setting them to null if there was an error
-    const photoResponse = results[0].status === 'fulfilled' ? results[0].value : null;
-    const userResponse = results[1].status === 'fulfilled' ? results[1].value : null;
-
-    // Returning an object with the responses
-    return {
-      photo: photoResponse,
-      user: userResponse,
-    };
+    return { photo: await uploadPhoto(), user: await createUser() };
   } catch (error) {
-    // Handle any unexpected errors
-    console.error('Error in asyncUploadUser:', error.message);
-    return {
-      photo: null,
-      user: null,
-    };
+    return { photo: null, user: null };
   }
-}
+};
+
+export default asyncUploadUser;
