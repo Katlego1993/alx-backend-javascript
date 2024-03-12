5272f8e2f5fdac868accfa606a5c71b1521b99da
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2024-03-12 11:20:32 +0000

    all

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,25 +1,8 @@
-export default function updateStudentGradeByCity(
-  students,
-  city,
-  newGrades,
-) {
-  const filterStudents = students.filter(
-    (student) => student.location === city,
-  );
-
-  const gradeStudents = filterStudents.map(
-    (student) => {
-      for (const gradeInfo of newGrades) {
-        if (student.id === gradeInfo.studentId) {
-          student.grade = gradeInfo.grade; 
-        }
-      }
-      if (student.grade === undefined) {
-        student.grade = 'N/A'; 
-      }
-      return student;
-    },
-  );
-
-  return gradeStudents;
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  return arr
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const grade = newGrades.find((g) => g.studentId === student.id);
+      return { ...student, grade: (grade && grade.grade) || 'N/A' };
+    });
 }
